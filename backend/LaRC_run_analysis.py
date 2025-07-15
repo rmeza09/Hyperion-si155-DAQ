b237fa6c7e9fc4be5db9b0a143e42f6efbb80c01
Rafael <rmeza@umd.edu>
2025-07-15 12:26:16 -0400

    expanded the analysis to the 19  experiments

--- backend/LaRC_run_analysis.py
+++ backend/LaRC_run_analysis.py
@@ -7,9 +7,27 @@ from matplotlib.ticker import ScalarFormatter
 from FFT_Analysis_Functions import fileReader, read_window, extract_impacts, apply_hamming_correction, single_fft_analysis
 
 
-#filePathVibe = './DATA/ENLIGHT/LaRC_test10_run0034.txt' 
-#filePathVibe = './DATA/ENLIGHT/LaRC_test9_run0032.txt' 
-filePathVibe = './DATA/ENLIGHT/LaRC_test8_run0030.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test1_run0008.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test2_run0010.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test3_run0012_bad.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test4_run0022_noKuliteData.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test5_run0024.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test6_run0026.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test7_run0028.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test8_run0030.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test9_run0032.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test10_run0034.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test11_run0036.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test12_run0038.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test13_run0040.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test14_run0042.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test15_run0044_5secIRdata.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test16_run046_noKuliteData.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test17_run0048.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test18_run0050.txt'
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test19_run0052_noIRdata.txt'
+
+filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test8_run0030.txt'
 #filePathTemp = './DATA/ENLIGHT/Peaks.20250325154942_TempTest.txt'
 
 import os
