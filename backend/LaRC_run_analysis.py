a319a5be07ac4904dfd9c7b57eba2c548380507b
Rafael <rmeza@umd.edu>
2025-07-17 17:39:16 -0400

    further analysis of experiments

--- backend/LaRC_run_analysis.py
+++ backend/LaRC_run_analysis.py
@@ -7,34 +7,29 @@ from matplotlib.ticker import ScalarFormatter
 from FFT_Analysis_Functions import fileReader, read_window, signal_detrend_analysis
 
 
-#filePathVibe = './DATA/LaRC_Data/LaRC_test1_run0008.txt' #set startTime = 180 timewWindow = 20
-filePathVibe = './DATA/LaRC_Data/LaRC_test2_run0010.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test3_run0012_bad.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test4_run0022_noKuliteData.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test5_run0024.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test6_run0026.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test7_run0028.txt'
-#filePathVibe = './DATA/LaRC_Data/LaRC_test8_run0030.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test9_run0032.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test10_run0034.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test11_run0036.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test12_run0038.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test13_run0040.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test14_run0042.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test15_run0044_5secIRdata.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test16_run046_noKuliteData.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test17_run0048.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test18_run0050.txt'
-# filePathVibe = './DATA/LaRC_Data/LaRC_test19_run0052_noIRdata.txt'
-
-#filePathVibe = './DATA/ENLIGHT/LaRC_Data/LaRC_test8_run0030.txt'
-#filePathTemp = './DATA/ENLIGHT/Peaks.20250325154942_TempTest.txt'
-
-
+# filePathVibe = './DATA/LaRC_Data/LaRC_test1_run0008.txt' #set startTime = 180 timewWindow = 20
+# filePathVibe = './DATA/LaRC_Data/LaRC_test2_run0010.txt' #set startTime = 210 timewWindow = 40
+# filePathVibe = './DATA/LaRC_Data/LaRC_test3_run0012_bad.txt' # not usable
+# filePathVibe = './DATA/LaRC_Data/LaRC_test4_run0022_noKuliteData.txt' # not usable
+# filePathVibe = './DATA/LaRC_Data/LaRC_test5_run0024.txt' #set startTime = 95 timewWindow = 15
+# filePathVibe = './DATA/LaRC_Data/LaRC_test6_run0026.txt' #set startTime = 175 timewWindow = 15
+# filePathVibe = './DATA/LaRC_Data/LaRC_test7_run0028.txt' #set startTime = 80 timewWindow = 15
+# filePathVibe = './DATA/LaRC_Data/LaRC_test8_run0030.txt' #set startTime = 180 timewWindow = 25
+# filePathVibe = './DATA/LaRC_Data/LaRC_test9_run0032.txt' #set startTime = 55 timewWindow = 15
+# filePathVibe = './DATA/LaRC_Data/LaRC_test10_run0034.txt' #set startTime = 80 timewWindow = 25
+filePathVibe = './DATA/LaRC_Data/LaRC_test11_run0036.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test12_run0038.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test13_run0040.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test14_run0042.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test15_run0044_5secIRdata.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test16_run046_noKuliteData.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test17_run0048.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test18_run0050.txt' #set startTime =  timewWindow = 
+# filePathVibe = './DATA/LaRC_Data/LaRC_test19_run0052_noIRdata.txt' #set startTime =  timewWindow = 
 
 
 startTime = 210
-timeWindow = 40
+timeWindow = 30
 cutoff = 100
 dfVibe, timeSpanV, samplingFreqV = fileReader(filePathVibe, False, True, start_trim=startTime)
 signal_detrend_analysis(filePathVibe, startTime, timeWindow, cutoff, plotSegments=False, plotFFT=True, saveFreqs=True, saveFigures=False)
